b9c2784d1f5a4f2a32f71d185e88e69469991bbc
Grace Kang <gkang@juul.com>
2019-05-31 15:38:19 -0700

    Fix app.rb

--- app.rb
+++ app.rb
@@ -1,22 +1,23 @@
-calc = Calculator.new
+require_relative 'lib/calculator.rb'
+
 puts "Roman numeral calculator:"
 loop do
   input = $stdin.gets
   arr = input.split(" ")
   if arr[1] == '+'
-    $stdout.write calc.add(arr[0], arr[2])
+    $stdout.write Calculator.add(arr[0], arr[2])
     $stdout.write "\n"
   elsif arr[1] == '-'
-    $stdout.write calc.sub(arr[0], arr[2])
+    $stdout.write Calculator.sub(arr[0], arr[2])
     $stdout.write "\n"
   elsif arr[1] == '*'
-    $stdout.write calc.mult(arr[0], arr[2])
+    $stdout.write Calculator.mult(arr[0], arr[2])
     $stdout.write "\n"
   elsif arr[1] == '/'
-    $stdout.write calc.div(arr[0], arr[2])
+    $stdout.write Calculator.div(arr[0], arr[2])
     $stdout.write "\n"
   elsif arr[1] == '%'
-    $stdout.write calc.mod(arr[0], arr[2])
+    $stdout.write Calculator.mod(arr[0], arr[2])
     $stdout.write "\n"
   end
 end
